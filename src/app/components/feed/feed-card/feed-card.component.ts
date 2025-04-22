bc2158043983187c1d80bd673a3040d2744c3cff
halex22 <halex2209@gmail.com>
2025-04-22 09:03:33 +0200

    add base some of the base structure to the feed reader

---
+++ src/app/components/feed/feed-card/feed-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './feed-card.component.html',
+  styleUrl: './feed-card.component.scss'
+})
+export class FeedCardComponent {
+
+}
---
+++ src/app/components/form/add-feed/add-feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-feed.component.html',
+  styleUrl: './add-feed.component.scss'
+})
+export class AddFeedComponent {
+
+}
