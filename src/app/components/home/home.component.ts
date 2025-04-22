1278269cfa205f0b3594c69839368477c9688025
MaNnu81 <mannuspamm@gmail.com>
2025-04-22 17:39:03 +0200

    +header + drawer (no css, no routing)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { AddFeedComponent } from "../form/add-feed/add-feed.component";
+import { MatIcon } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatSidenavModule, AddFeedComponent, MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  showFiller = false;
 
+  isSidebarOpen = input(false);
 }
