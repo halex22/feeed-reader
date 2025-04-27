836d31f7e22d6465a9f9e8507e8cf1f09b7eb27d
MaNnu81 <mannuspamm@gmail.com>
2025-04-27 22:13:02 +0200

    rout  det ok. no fetch

--- src/app/components/feed/detail/detail.component.ts
+++ src/app/components/feed/detail/detail.component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Input } from '@angular/core';
 import { FeedService } from '../../../services/feed.service';
+import { RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
 export class DetailComponent {
   @Input() feedName!: string 
 
-  constructor(private feedService: FeedService, private route: ActivatedRoute) {
-    this.feedService.fetchFeedNews(this.feedName)
+  constructor(private feedService: FeedService, private route: ActivatedRoute ) {
+    this.route.params.subscribe(params => {
+      this.feedName = params['feedName'];
+      this.feedService.fetchFeedNews(this.feedName)
     .then(data => console.log(data)) // questo metodo ti torna una promesa con le news poi vanno messe in un attr della classe per essere usati nel HTML
-  }
+  });
+
   // questo deve essere il nome del parametro della ruta dinamica
   // {
   //   path: 'feed/:feedName',
   //   component: DetailComponent
-  // }
+   }
 }
